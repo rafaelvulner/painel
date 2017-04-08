35d0de7b1921f465a552bac7ce7c1c01aa646678
rafael <rafaell.elias@hotmail.com>
2017-04-08 02:22:29 -0300

    celera

--- build/generated-sources/ap-source-output/br/com/painel/modelo/Cadastro_.java
+++ build/generated-sources/ap-source-output/br/com/painel/modelo/Cadastro_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-08T02:06:58")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-08T02:20:18")
 @StaticMetamodel(Cadastro.class)
 public class Cadastro_ { 
 
