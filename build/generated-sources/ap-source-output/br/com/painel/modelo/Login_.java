b86ad05c07801d381c23f5fd053e6e10fc3da20f
rafael <rafaell.elias@hotmail.com>
2017-04-08 02:08:47 -0300

    atts

--- build/generated-sources/ap-source-output/br/com/painel/modelo/Login_.java
+++ build/generated-sources/ap-source-output/br/com/painel/modelo/Login_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-08T01:17:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-08T02:06:58")
 @StaticMetamodel(Login.class)
 public class Login_ { 
 
